78e19286de0ea1006d98ad503b5b23d0f2ffc16e
Jose Angarita <amjose09@gmail.com>
2022-01-07 10:49:20 -0500

    archivo que contiene las rutas de auth

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { DbConnection } from "./db/db.js";
+import { authRouter } from "./routes/auth.js";
 
 dotenv.config()
 
@@ -9,6 +10,8 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/auth',authRouter)
 
 app.get("/", (req, res) => {
     res.json({
