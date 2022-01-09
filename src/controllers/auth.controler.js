3d0de18366a9452be9ea45ede9e0478befde09b9
Jose Angarita <amjose09@gmail.com>
2022-01-09 09:51:00 -0500

    Backend con mongoose antes de pasarlo a prisma

--- src/controllers/auth.controler.js
+++ src/controllers/auth.controler.js
@@ -1,9 +1,26 @@
+import { validationResult } from "express-validator"
+
+// TENGO QUE VER 10.EXPRESS-VALIDATOR MIN: 10:10 va a aplicar las validaciones al email
+
 export const registerUser = (req, res) => {
-    const {name,email,password} = req.body
-    res.status(200).json({
-        message:"User created successfully",
+
+    const {name,email,password} = req.body 
+
+    const errors = validationResult( req )
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            ok:false,
+            errors:errors.mapped()
+        })
+    }
+
+    res.status(201).json({
+        ok:true,
+        message:"User created succesfully",
         data:{name,email,password}
     })
+
 }
 
 export const loginUser = (req, res) => {
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,12 +4,26 @@
 */
 
 import express from 'express';
-import { loginUser, refreshToken, registerUser } from '../controllers/auth.controler.js';
+import {
+    check
+} from 'express-validator';
+import {
+    loginUser,
+    refreshToken,
+    registerUser
+} from '../controllers/auth.controler.js';
 
 export const authRouter = express.Router()
 
-authRouter.get("/new", refreshToken)
+authRouter.get("/new",refreshToken)
 
-authRouter.post("/register",registerUser)
+authRouter.post(
+    "/register",
+    [
+        check('name', "User name must be provided").not().isEmpty(),
+        check('email', "Email must be provided").isEmail(),
+        check('password', "Password length must be at least 6 characters").isLength({min:6})
+    ],
+    registerUser)
 
 authRouter.post("/", loginUser)
